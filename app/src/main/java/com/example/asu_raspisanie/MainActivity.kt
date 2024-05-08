0b1c4482f363b1406069b0556d8e01f876cefc93
Oringom <a-stud-z@yandex.ru>
2024-05-08 16:29:31 +0700

    НЕ БРАТЬ!

--- app/src/main/java/com/example/asu_raspisanie/MainActivity.kt
+++ app/src/main/java/com/example/asu_raspisanie/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Ты че, долбаеб?",
+        text = "Здравствуйте",
         modifier = modifier
     )
 }
